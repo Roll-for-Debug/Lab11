9022a08f435a3a4d48eb4848d6185340e0b3d61c
Colin Hanway <hanway.colin@gmail.com>
2025-04-29 09:45:41 -0600

    init Lab11
    add files

---
+++ IUArrayList.java
@@ -0,0 +1,335 @@
+import java.util.*;
+
+/**
+ * Array-based implementation of IndexedUnsortedList.
+ *
+ * @author
+ *
+ * @param <E> type to store
+ */
+public class IUArrayList<E> implements IndexedUnsortedList<E> {
+	private static final int DEFAULT_CAPACITY = 10;
+	private static final int NOT_FOUND = -1;
+
+	private E[] array;
+	private int rear;
+	private int modCount; // DO NOT REMOVE ME
+
+	/** Creates an empty array with default initial capacity */
+	public IUArrayList() {
+		this(DEFAULT_CAPACITY);
+	}
+
+	/**
+	 * Creates an empty array with the given initial capacity
+	 * 
+	 * @param initialCapacity
+	 */
+	@SuppressWarnings("unchecked")
+	public IUArrayList(int initialCapacity) {
+		array = (E[]) (new Object[initialCapacity]);
+		rear = 0;
+		modCount = 0; // DO NOT REMOVE ME
+	}
+
+	/** Double the capacity of array */
+	private void expandCapacity() {
+		array = Arrays.copyOf(array, array.length * 2);
+	}
+
+	@Override
+	public void addToFront(E element) {
+		// REVIEW Zion
+		// if (this.size() == this.array.length) { expandCapacity(); } - CH: Included in
+		// shiftEntries
+		add(0, element);
+		modCount++; // DO NOT REMOVE ME
+	}
+
+	@Override
+	public void addToRear(E element) {
+		// REVIEW Colin
+		add(rear, element);
+		modCount++; // DO NOT REMOVE ME
+	}
+
+	@Override
+	public void add(E element) {
+		// REVIEW Tyler
+		add(rear, element);
+		modCount++; // DO NOT REMOVE ME
+	}
+
+	@Override
+	public void addAfter(E element, E target) {
+		// REVIEW Kelsi
+		emptyCheck();
+		if (!contains(target)) {
+			throw new NoSuchElementException();
+		}
+		add(indexOf(target) + 1, element);
+		modCount++; // DO NOT REMOVE ME
+	}
+
+	@Override
+	public void add(int index, E element) {
+		// TODO Tyra
+		rangeCheck(index);
+		shiftEntries(index);
+		array[index] = element;
+		modCount++; // DO NOT REMOVE ME
+	}
+
+	@Override
+	public E removeFirst() {
+		// REVIEW Zion
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		E retVal = this.remove(first());
+		modCount++; // DO NOT REMOVE ME
+		return retVal;
+	}
+
+	@Override
+	public E removeLast() {
+		// REVIEW Colin
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		E retVal = this.remove(rear - 1);
+		modCount++; // DO NOT REMOVE ME
+		return retVal;
+	}
+
+	@Override
+	public E remove(E element) {
+		int index = indexOf(element);
+		if (index == NOT_FOUND) {
+			throw new NoSuchElementException();
+		}
+
+		E retVal = array[index];
+
+		rear--;
+		// shift elements
+		for (int i = index; i <= rear; i++) {
+			array[i] = array[i + 1];
+		}
+		array[rear] = null;
+
+		modCount++; // DO NOT REMOVE ME
+		return retVal;
+	}
+
+	public E remove(int index) {
+		// REVIEW Tyler
+		if (index < 0 || index >= rear) { // ***CH- Corrected corrections***
+			throw new IndexOutOfBoundsException();
+		}
+
+		E retVal = array[index];
+
+		rear--;
+		// shift elements
+		for (int i = index; i <= rear; i++) {
+			array[i] = array[i + 1];
+		}
+		array[rear] = null;
+
+		modCount++; // DO NOT REMOVE ME
+		return retVal;
+	}
+
+	@Override
+	public void set(int index, E element) {
+		// REVIEW Kelsi
+		if (index < 0 || index >= rear) {
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + rear);
+		}
+		array[index] = element;
+		modCount++; // DO NOT REMOVE ME
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Tyra
+		if (index < 0 || index >= rear) {
+			throw new IndexOutOfBoundsException();
+		}
+		return array[index];
+	}
+
+	@Override
+	public int indexOf(E element) {
+		if (isEmpty()) {
+			return NOT_FOUND;
+		}
+		for (int i = 0; i < rear; i++) {
+			if (element.equals(array[i])) {
+				return i;
+			}
+		}
+
+		return NOT_FOUND;
+	}
+
+	@Override
+	public E first() {
+		// REVIEW Zion
+		emptyCheck();
+		return array[0];
+	}
+
+	@Override
+	public E last() {
+		// REVIEW Colin
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		return get(rear - 1);
+	}
+
+	@Override
+	public boolean contains(E target) {
+		return (indexOf(target) != NOT_FOUND);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// REVIEW Tyler
+		return size() == 0;
+	}
+
+	@Override
+	public int size() {
+		// REVIEW Kelsi
+		return rear;
+	}
+
+	@Override
+	public String toString() {
+		String result = "[";
+		// TODO Tyra
+		for (E e : array) {
+			if (e != null) {
+				result += e + ", ";
+			}
+		}
+		return result + "]";
+	}
+
+	private void shiftEntries(int startingIndex) {
+		if (rear == array.length) {
+			expandCapacity();
+		}
+		for (int i = rear; i > startingIndex && i != 0; i--) {
+			array[i] = array[i - 1];
+		}
+		this.rear++;
+
+	}
+
+	private void emptyCheck() {
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+	}
+
+	private void rangeCheck(int index) {
+		if (index < 0 || index > rear) {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+	// POSSIBLY MORE CORRECT IMPLMENTATION FOR THIS ONE?? ~ Kelsi
+	// if (rear == array.length) {
+	// expandCapacity();
+	// }
+	// for (int i = rear; i > startingIndex; i--) {
+	// array[i] = array[i - 1];
+	// }
+	// rear++;
+	// }
+
+	// IGNORE THE FOLLOWING COMMENTED OUT CODE UNTIL LAB 10
+	// DON'T DELETE ME, HOWEVER!!!
+	public Iterator<E> iterator() {
+		// return new IUArrayarrayIterator(); // UNCOMMENT ME IN LAB 10
+		return null; // REMOVE ME IN LAB 10
+	}
+
+	// UNCOMMENT THE CODE BELOW IN LAB 10
+
+	// private class IUArrayarrayIterator implements Iterator<E> {
+
+	// private int iterModCount, current;
+	// private boolean canRemove;
+
+	// public IUArrayarrayIterator() {
+	// iterModCount = modCount;
+	// current = 0;
+	// canRemove = false;
+	// }
+
+	// @Override
+	// public boolean hasNext() {
+	// if (iterModCount != modCount) {
+	// throw new ConcurrentModificationException();
+	// }
+	// return current < rear;
+	// }
+
+	// @Override
+	// public E next() {
+	// if (!hasNext()) {
+	// throw new NoSuchElementException();
+	// }
+	// E item = array[current];
+	// current++;
+	// canRemove = true;
+	// return item;
+	// }
+
+	// @Override
+	// public void remove() {
+	// if (iterModCount != modCount) {
+	// throw new ConcurrentModificationException();
+	// }
+	// if (!canRemove) {
+	// throw new IllegalStateException();
+	// }
+	// // remove the element in the array at index current-1
+	// // presumably decrement the rear
+	// // presumably the modCount is getting incremented
+	// // all indices have to back up by one
+	// current--;
+	// rear--;
+	// // shift elements to the left
+	// for (int i = current; i < rear; i++) {
+	// array[i] = array[i + 1];
+	// }
+	// array[rear] = null;
+	// modCount++;
+	// iterModCount++;
+	// // Can only remove the LAST "seen" element
+	// // set back to a non-removal state
+	// canRemove = false;
+	// }
+
+	// }
+
+	// IGNORE THE FOLLOWING CODE
+	// DON'T DELETE ME, HOWEVER!!!
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int startingIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ IUSingleLinkedList.java
@@ -0,0 +1,361 @@
+import java.util.*;
+
+/**
+ * Single-linked node implementation of IndexedUnsortedList.
+ * An Iterator with working remove() method is implemented, but
+ * ListIterator is unsupported.
+ *
+ * @author
+ *
+ * @param <E> type to store
+ */
+public class IUSingleLinkedList<E> implements IndexedUnsortedList<E> {
+	private LinearNode<E> front, rear;
+	private int count;
+	private int modCount;
+
+	/** Creates an empty list */
+	public IUSingleLinkedList() {
+		front = rear = null;
+		count = 0;
+		modCount = 0;
+	}
+
+	@Override
+	public void addToFront(E element) { // Colin
+		LinearNode<E> node = new LinearNode<E>(element);
+		if (isEmpty()) {
+			front = rear = node;
+			count++;
+			return;
+		}
+		node.setNext(front);
+		front = node; // One for the garbage man
+		count++;
+		modCount++;
+	}
+
+	@Override
+	public void addToRear(E element) { // Zion
+		LinearNode<E> newNode = new LinearNode<E>(element);
+		if (isEmpty()) { // add isEmpty check as emptyList rear is null and rear.setNext won't work
+			front = rear = newNode;
+			count++;
+		} else {
+			rear.setNext(newNode);
+			rear = rear.getNext();
+			newNode = null;
+			count++;
+		}
+	}
+
+	@Override
+	public void add(E element) {
+		// Review Tyler
+		addToRear(element);
+	}
+
+	@Override
+	public void addAfter(E element, E target) { // Kelsi
+		LinearNode<E> current = front;
+		LinearNode<E> newNode = new LinearNode<E>(element);
+
+		while (current != null) {
+			if (current.getElement().equals(target)) {
+				newNode.setNext(current.getNext());
+				current.setNext(newNode);
+
+				if (current == rear) {
+					rear = newNode;
+				}
+				count++;
+				modCount++;
+				return;
+			}
+			// Next Node
+			current = current.getNext();
+		}
+		throw new NoSuchElementException();
+
+	}
+
+	@Override
+	public void add(int index, E element) {
+		// TODO Tyra
+		LinearNode<E> current = front;
+		LinearNode<E> addMe = new LinearNode<E>(element);
+		if(index == 0) {
+			addToFront(element);
+			return;
+		}
+		if (index < 0 || index > size()) { throw new IndexOutOfBoundsException(); }
+		for (int i = 0; i < index - 1; i++) {
+			current = current.getNext();
+		}
+		if (current.getNext() == null) {
+			current.setNext(addMe);
+			rear = addMe;
+		} else {
+			addMe.setNext(current.getNext());
+			current.setNext(addMe);
+		}
+		count++;
+		modCount++;
+
+
+
+	}
+
+	@Override
+	public E removeFirst() { // Colin
+		if (front == null) {
+			throw new NoSuchElementException();
+		}
+		return remove(front.getElement());
+	}
+
+	/*
+	 * - Kelsi
+	 * Will the return front.getElement(); throw NullPointerException?
+	 * - Colin
+	 * Good catch, was returning the wrong element, only NPE on single element list
+	 * worked in my testing surprisingly.
+	 */
+
+	@Override
+	public E removeLast() { // Zion
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		return remove(rear.getElement());
+	}
+
+	@Override
+	public E remove(E element) { // this was given to us do not touch
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		LinearNode<E> current = front, previous = null;
+		while (current != null && !current.getElement().equals(element)) {
+			previous = current;
+			current = current.getNext();
+		}
+		// Matching element not found
+		if (current == null) {
+			throw new NoSuchElementException();
+		}
+		return removeElement(previous, current);
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Kelsi
+		if (index > count || index < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		return remove(get(index));
+	}
+
+	@Override
+	public void set(int index, E element) {
+		// TODO Tyra
+		if (isEmpty()) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index < 0 || index > count) {
+			throw new IndexOutOfBoundsException();
+		}
+		LinearNode<E> current = front;
+		int i = 0;
+		while (current != null && i < index) {
+			current = current.getNext();
+			i++;
+		}
+		if (current == null) {
+			throw new IndexOutOfBoundsException();
+		}
+		current.setElement(element);
+	}
+
+	@Override
+	public E get(int index) { // Colin
+		if (isEmpty()) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index < 0 || index > count) {
+			throw new IndexOutOfBoundsException();
+		}
+		LinearNode<E> current = front;
+		int i = 0;
+		while (current != null && i < index) {
+			current = current.getNext();
+			i++;
+		}
+		if (current == null) {
+			throw new IndexOutOfBoundsException();
+		} // Not sure if this is necessary - Colin
+		modCount++;
+		return current.getElement();
+	}
+
+	@Override
+	public int indexOf(E element) { // Zion
+		if (isEmpty()) {
+			return -1;
+		}
+		LinearNode<E> temp = this.front;
+		int indexCounter = 0;
+		do {
+			if (temp.getElement().equals(element)) {
+				return indexCounter;
+			}
+			temp = temp.getNext();
+			indexCounter++;
+			if (temp == null) {
+				return -1;
+			}
+		} while (!(temp == null));
+		return -1;
+	}
+
+	@Override
+	public E first() {
+		// Review Tyler
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		return front.getElement();
+	}
+
+	@Override
+	public E last() {
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		// TODO Kelsi
+		return rear.getElement();
+	}
+
+	@Override
+	public boolean contains(E target) {
+		// TODO Tyra
+		return indexOf(target) != -1;
+	}
+
+	@Override
+	public boolean isEmpty() { // Colin
+		return count == 0;
+	}
+
+	@Override
+	public int size() { // Zion
+		return count;
+	}
+
+	@Override
+	public String toString() {
+		// Review Tyler
+		String result = "[";
+		LinearNode<E> current = front;
+		while (current != null) {
+			result += current.getElement();
+			current = current.getNext();
+			if (current != null) {
+				result += ", ";
+			}
+		}
+		return result += "]";
+	}
+
+	private E removeElement(LinearNode<E> previous, LinearNode<E> current) { // given, don't much
+		// Grab element
+		E result = current.getElement();
+		// If not the first element in the list
+		if (previous != null) {
+			previous.setNext(current.getNext());
+		} else { // If the first element in the list
+			front = current.getNext();
+		}
+		// If the last element in the list
+		if (current.getNext() == null) {
+			rear = previous;
+		}
+		count--;
+		modCount++;
+		// System.out.println(result);
+		return result;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new SLLIterator();
+	}
+
+	/** Iterator for IUSingleLinkedList */
+	private class SLLIterator implements Iterator<E> {
+		private LinearNode<E> previous;
+		private LinearNode<E> current;
+		private LinearNode<E> next;
+		private int iterModCount;
+		private boolean didNext;
+
+		/** Creates a new iterator for the list */
+		public SLLIterator() {
+			previous = null;
+			current = null;
+			next = front;
+			iterModCount = modCount;
+			didNext = false;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Kelsi
+			return next != null;
+		}
+
+		@Override
+		public E next() {
+			// TODO Tyra
+			if (iterModCount != modCount) { throw new ConcurrentModificationException(); }
+			if (next == null) { throw new NoSuchElementException(); }
+
+			previous = current;
+			current = next;
+			if (current.getNext() != null){
+				next = next.getNext();
+			} else {
+				next = null;
+			}
+			didNext = true; // Allow remove
+			// System.out.println("__CURRENT__:" + current.getElement());
+			return current.getElement();
+		}
+
+		@Override
+		public void remove() { // Colin
+			if (!didNext) { throw new IllegalStateException(); }
+			if (current == front) {
+				removeFirst();
+			} else if (current == rear) {
+				removeLast();
+			} else {
+				removeElement(previous, current);
+			}
+			didNext = false; // disallow remove until next, next()
+			iterModCount++;
+		}
+	}
+
+	// IGNORE THE FOLLOWING CODE
+	// DON'T DELETE ME, HOWEVER!!!
+	@Override
+	public ListIterator<E> listIterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int startingIndex) {
+		throw new UnsupportedOperationException();
+	}
+}
