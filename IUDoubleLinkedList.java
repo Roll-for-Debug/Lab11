4440ce7afde398d005198a8621814dee918e5342
chanway <hanway.colin@gmail.com>
2025-04-29 11:50:50 -0600

    Fixed the Object calls

--- IUDoubleLinkedList.java
+++ IUDoubleLinkedList.java
@@ -4,91 +4,91 @@ import java.util.ListIterator;
 public class IUDoubleLinkedList<E> implements IndexedUnsortedList<E> {
 
     @Override
-    public void addToFront(Object element) { // Colin
+    public void addToFront(E element) { // Colin
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addToFront'");
     }
 
     @Override
-    public void addToRear(Object element) { // Zion
+    public void addToRear(E element) { // Zion
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addToRear'");
     }
 
     @Override
-    public void add(Object element) { // Tyler
+    public void add(E element) { // Tyler
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
 
     @Override
-    public void addAfter(Object element, Object target) { // Kelsi
+    public void addAfter(E element, E target) { // Kelsi
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addAfter'");
     }
 
     @Override
-    public void add(int index, Object element) { // Tyra
+    public void add(int index, E element) { // Tyra
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
 
     @Override
-    public Object removeFirst() { // Colin
+    public E removeFirst() { // Colin
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
     }
 
     @Override
-    public Object removeLast() { // Zion
+    public E removeLast() { // Zion
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
     }
 
     @Override
-    public Object remove(Object element) { // Tyler
+    public E remove(E element) { // Tyler
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'remove'");
     }
 
     @Override
-    public Object remove(int index) { // Kelsi
+    public E remove(int index) { // Kelsi
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'remove'");
     }
 
     @Override
-    public void set(int index, Object element) { // Tyra
+    public void set(int index, E element) { // Tyra
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'set'");
     }
 
     @Override
-    public Object get(int index) { // Colin
+    public E get(int index) { // Colin
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'get'");
     }
 
     @Override
-    public int indexOf(Object element) { // Zion
+    public int indexOf(E element) { // Zion
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'indexOf'");
     }
 
     @Override
-    public Object first() { // Tyler
+    public E first() { // Tyler
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'first'");
     }
 
     @Override
-    public Object last() { // Kelsi
+    public E last() { // Kelsi
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'last'");
     }
 
     @Override
-    public boolean contains(Object target) { // Tyra
+    public boolean contains(E target) { // Tyra
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'contains'");
     }
